3c9a42d00ef7edf49589557f8ac932f0071959c9
Filip Kasaj <filip.kasaj@gmail.com>
2022-11-10 09:05:06 +0100

    ValueFromPipeline added

--- PowerShell/Transpose-Object.ps1
+++ PowerShell/Transpose-Object.ps1
@@ -1,16 +1,28 @@
-Function Transpose-Object ($Objects, $Delimiter){
-    $Props = $Objects | Get-Member -MemberType Property,NoteProperty|%{$_.name}
-    $Report = [System.Collections.Generic.List[Object]]::new()
-    foreach($Object in $Objects){
-        foreach($Prop in $Props){
-            $ReportLine = [PSCustomObject] @{
-                PropertyName = $Prop
-                PropertyValue = $Object.$Prop -join ($Delimiter)
+Function Transpose-Object {
+    [CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipeline)] 
+        [PsObject]$Object,
+        [string] $Delimiter
+    )
+    Begin{
+        $Out = [System.Collections.Generic.List[Object]]::new()
+    }
+    Process{
+        $Props = $Object|Get-Member -MemberType property,NoteProperty|%{$_.name}
+        foreach($Obj in $Object){
+            foreach($Prop in $Props){
+                $Line = [PSCustomObject] @{
+                    PropertyName = $Prop
+                    PropertyValue = $Obj.$Prop -join ($Delimiter)
+                }
+                $Out.Add($Line)
             }
-            $Report.Add($ReportLine)
         }
     }
-    $Report
+    End{
+        $Out
+    }
 }
 
 <# EXAMPLE
@@ -22,7 +34,7 @@ DisplayName                  Members
 Test Group 1                 {GradyA, ChristieC, AlexW}
 Test Group 2                 {AlexW}
 
-PS C:\> Transpose-Object (Get-Group "test *"|select DisplayName,Members ) (", ")
+PS C:\> Get-Group "test *"|select DisplayName,Members |Transpose-Object -Delimiter (", ")
 
 PropertyName PropertyValue
 ------------ -------------
@@ -42,20 +54,20 @@ TargetServicePlan           :
 InPlaceHolds                : {grp29d1313ebf2742b896e2ac057e619a81:1, mbx29d1313ebf2742b896e2ac057e619a81:1, mbx8e3818129bcd42298ffeb3e4fb0e3914:1, grp8e3818129bcd42298ffeb3e4fb0e3914:1}    
 IsMIPLabelForGroupsEnabled  : False
 
-PS C:\> Transpose-Object (Get-OrganizationConfig|select *pla*) (", ")
+PS C:\> Get-OrganizationConfig|select *pla* | Transpose-Object -Delimiter ("|")
 
 PropertyName                PropertyValue
 ------------                -------------
 AdminDisplayVersion         0.20 (15.20.5813.11)
 DisplayName                 Contoso
-InPlaceHolds                grp29d1313ebf2742b896e2ac057e619a81:1, mbx29d1313ebf2742b896e2ac057e619a81:1, mbx8e3818129bcd42298ffeb3e4fb0e3914:1, grp8e3818129bcd42298ffeb3e4fb0e3914:1        
+InPlaceHolds                grp29d1313ebf2742b896e2ac057e619a81:1|mbx29d1313ebf2742b896e2ac057e619a81:1|mbx8e3818129bcd42298ffeb3e4fb0e3914:1|grp8e3818129bcd42298ffeb3e4fb0e3914:1
 IsMIPLabelForGroupsEnabled  False
 IsUpdatingServicePlan       False
 PreviousAdminDisplayVersion 0.10 (14.0.100.0)
 ServicePlan                 BPOS_S_E15_0
 TargetServicePlan
 
-PS C:\> Transpose-Object (Get-OrganizationConfig|select *pla*) (", ") | Export-Csv -Path "Test" -Delimiter "`t" -NoTypeInformation -Encoding unicode
+PS C:\> Get-OrganizationConfig|select *pla* | Transpose-Object -Delimiter ("|") | Export-Csv -Path "Test" -Delimiter "`t" -NoTypeInformation -Encoding unicode
 
 #>
 
